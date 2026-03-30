f93a7078beb9446e3ca2ee83456927b5a84aaa77
Serena Chan <serenaktc@gmail.com>
2026-03-30 16:31:23 -0400

    edited from seb meeting abt class imbalance

--- scripts/finetune/ft_main.jl
+++ scripts/finetune/ft_main.jl
@@ -3,7 +3,7 @@ Pkg.activate("/home/golem/scratch/chans/lincsv3")
 #TODO: likely don't need aarch64 config here because its kinda deezed for this task?
 Pkg.instantiate()
 
-using LincsProject, JLD2, Flux, Optimisers, ProgressBars, Statistics, CUDA, Dates
+using LincsProject, JLD2, Flux, Optimisers, ProgressBars, Statistics, CUDA, Dates, cuDNN, StatsBase
 
 include("../../src/params.jl")
 include("../../src/fxns.jl")
@@ -13,13 +13,20 @@ include("train.jl")
 
 # settings
 
-level = "lvl1"
+level = "lvl2"
 modeltype = "rtf"
+pt1_epochs = 3
+pt2_epochs = 12
+additional_notes = "tryign first lvl1 w/ new model"
+
+use_pca = modeltype in ("v1", "v2")
+use_oversampling = level == "lvl2"
+
 include("$(modeltype)_structs.jl")
 if modeltype == "rtf"
-    dir = "/home/golem/scratch/chans/lincsv3/plots/trt/rank_tf/2026-03-14_13-58"
+    dir = "/home/golem/scratch/chans/lincsv3/plots/trt/rank_tf/2026-03-24_02-55"
 elseif modeltype == "v1"
-    dir = "/home/golem/scratch/chans/lincsv3/plots/trt/rtf_v1/2026-03-11_10-42"
+    dir = "/home/golem/scratch/chans/lincsv3/plots/trt/rtf_v1/2026-03-11_10-42" #TODO: change after v1 finished running
 elseif modeltype == "v2"
     dir = nothing #TODO: change after v2 finished running
 else
@@ -35,10 +42,6 @@ else
     error("check ur gpu!!!")
 end
 
-pt1_epochs = 1
-pt2_epochs = 1
-additional_notes = "tryign first lvl3"
-
 # if testing
 # data_path = "data/lincs_untrt_data.jld2"
 # dataset = "untrt
@@ -52,8 +55,17 @@ X = data.expr
 
 if level == "lvl1"
     y = data.inst.cell_mfc_name # or is it cell_iname?
+
 elseif level == "lvl2"
     y = data.inst.pert_id # pert_type = trt vs untrt, pert_id = actual perturbation type
+
+    counts = countmap(y)
+    sorted = sort(collect(counts), by=last, rev=true)
+    top500 = [k for (k,v) in sorted[1:500]] |> Set
+    idx = findall(l -> l in top500, y)
+    X = X[:, idx] # 978 x 484067
+    y = y[idx]
+
 elseif level == "lvl3"
     y = nothing # TODO: what even is this task
 
@@ -75,7 +87,7 @@ y_oh = Flux.onehotbatch(y_ids, 1:n_classifications)
 
 n_genes = size(X, 1) 
 n_classes_pt = n_genes
-n_features_pt = n_classes_pt + 2 
+n_features_pt = n_classes_pt + 1
 # CLS_ID = n_features_pt 
 
 gene_medians = vec(median(X, dims=2)) .+ 1e-10
@@ -89,13 +101,28 @@ X_train, X_test, train_indices, test_indices = split_data(X_ranked, 0.2)
 y_train = y_oh[:, train_indices]
 y_test = y_oh[:, test_indices]
 
-# pca stuff
-raw_train = X[:, train_indices]
-raw_test = X[:, test_indices]
-raw_train_norm = StatsBase.zscore(Float32.(raw_train), 2)
-raw_test_norm = StatsBase.zscore(Float32.(raw_test), 2)
-pca_train_norm = fit(PCA, Float32.(raw_train_norm); maxoutdim=embed_dim);
+if use_pca
+    raw_train = X[:, train_indices]
+    raw_test = X[:, test_indices]
+    raw_train_norm = StatsBase.zscore(Float32.(raw_train), 2)
+    raw_test_norm = StatsBase.zscore(Float32.(raw_test), 2)
+    pca_train_norm = fit(PCA, Float32.(raw_train_norm); maxoutdim=embed_dim)
+end
 
+if use_oversampling
+    y_train_labels = Flux.onecold(cpu(y_train))
+    cidx_dict = Dict{Int, Vector{Int}}()
+    for (idx, class) in enumerate(y_train_labels)
+        push!(get!(cidx_dict, class, Int[]), idx)
+    end
+    cs = collect(keys(cidx_dict))
+else
+    cidx_dict, cs = nothing, nothing
+end
+
+function oversample_batch(class_dict, classes, b_size)
+    return [rand(class_dict[rand(classes)]) for _ in 1:b_size]
+end
 
 state = load("$dir/model_state.jld2")["model_state"]
 general_model = (
@@ -106,6 +133,7 @@ general_model = (
     n_heads=n_heads,
     hidden_dim=hidden_dim,
     dropout_prob=drop_prob)
+
 if modeltype == "rtf"
     pt_model = Model(; general_model...) |> gpu
 elseif modeltype == "v1"
@@ -131,7 +159,7 @@ pt1_test_losses = Float32[]
 pt1_preds = Int[]
 pt1_trues = Int[]
 
-train(pt1_epochs, pt1_train_losses, pt1_test_losses, pt1_preds, pt1_trues, ce_loss)
+train(pt1_epochs, pt1_train_losses, pt1_test_losses, pt1_preds, pt1_trues, ce_loss, use_oversampling, cidx_dict, cs, use_pca, batch_size)
 
 # pt2: gradient updates both transformer and classifier weights
 
@@ -143,7 +171,7 @@ pt2_test_losses = Float32[]
 pt2_preds = Int[]
 pt2_trues = Int[]
 
-train(pt2_epochs, pt2_train_losses, pt2_test_losses, pt2_preds, pt2_trues, ce_loss)
+train(pt2_epochs, pt2_train_losses, pt2_test_losses, pt2_preds, pt2_trues, ce_loss, use_oversampling, cidx_dict, cs, use_pca, batch_size)
 
 # log stuff
 
@@ -191,3 +219,15 @@ open(params_txt, "w") do io
     println(io, "ADDITIONAL NOTES: $additional_notes")
     println(io, "run_time = $(run_hours) hours and $(run_minutes) minutes")
 end
+
+### explore: reduce number of classes
+
+cls = countmap(data.inst.pert_id)
+fcls = filter(cls) do p
+    p.second > 1000 && p.second < 20000
+end |> Dict
+
+sum(p -> p.second, fcls)
+
+df = DataFrame(k=collect(keys(fcls)), v=collect(values(fcls)))
+sort(df, :v)
